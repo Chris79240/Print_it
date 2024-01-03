f2986e3c1a7b03960d6a7b1dd7839a98d89f6c4b
Chris79240 <oservice.fr@gmail.com>
2024-01-03 21:06:08 +0100

    Flèches du carousel

--- assets/script.js
+++ assets/script.js
@@ -19,35 +19,35 @@ const slides = [
   ];
   
  // Définition des slides
-const slides = Array.from(document.querySelectorAll('.slide')); // Utilisez la méthode Array.from pour convertir la NodeList en un tableau
+ const slides = Array.from(document.querySelectorAll('.slide')); // Utilisez la méthode Array.from pour convertir la NodeList en un tableau
 
-// Variables
-let activeSlideIndex = 0; // Index de la slide active
-
-// Fonction pour mettre à jour les slides
-function updateSlides() {
-  slides.forEach((slide, index) => {
-    if (index === activeSlideIndex) {
-      slide.classList.add('active'); // Ajouter la classe "active" à la slide active
-    } else {
-      slide.classList.remove('active'); // Supprimer la classe "active" des autres slides
-    }
-  });
-}
-
-// Initialisation des slides
-updateSlides();
-
-// Écouteur d'événement pour le bouton de navigation vers la droite
-const arrowRightButton = document.getElementById('arrow_right');
-arrowRightButton.addEventListener('click', function() {
-  activeSlideIndex = (activeSlideIndex + 1) % slides.length; // Passer à la slide suivante (en boucle)
-  updateSlides(); // Mettre à jour l'affichage des slides
-});
-
-// Écouteur d'événement pour le bouton de navigation vers la gauche
-const arrowLeftButton = document.getElementById('arrow_left');
-arrowLeftButton.addEventListener('click', function() {
-  activeSlideIndex = (activeSlideIndex - 1 + slides.length) % slides.length; // Passer à la slide précédente (en boucle)
-  updateSlides(); // Mettre à jour l'affichage des slides
-});
+ // Variables
+ let activeSlideIndex = 0; // Index de la slide active
+ 
+ // Fonction pour mettre à jour les slides
+ function updateSlides() {
+   slides.forEach((slide, index) => {
+     if (index === activeSlideIndex) {
+       slide.classList.add('active'); // Ajouter la classe "active" à la slide active
+     } else {
+       slide.classList.remove('active'); // Supprimer la classe "active" des autres slides
+     }
+   });
+ }
+ 
+ // Initialisation des slides
+ updateSlides();
+ 
+ // Écouteur d'événement pour le bouton de navigation vers la droite
+ const arrowRightButton = document.getElementById('arrow_right');
+ arrowRightButton.addEventListener('click', function() {
+   activeSlideIndex = (activeSlideIndex + 1) % slides.length; // Passer à la slide suivante (en boucle)
+   updateSlides(); // Mettre à jour l'affichage des slides
+ });
+ 
+ // Écouteur d'événement pour le bouton de navigation vers la gauche
+ const arrowLeftButton = document.getElementById('arrow_left');
+ arrowLeftButton.addEventListener('click', function() {
+   activeSlideIndex = (activeSlideIndex - 1 + slides.length) % slides.length; // Passer à la slide précédente (en boucle)
+   updateSlides(); // Mettre à jour l'affichage des slides
+ });
